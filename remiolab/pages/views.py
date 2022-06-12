de599af3325bcfae0a4ea0e62335e53eef5ea4ff
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-06-12 10:50:08 -0500

    views updated

--- remiolab/pages/views.py
+++ remiolab/pages/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import DetailView, RedirectView, UpdateView, TemplateView, CreateView
+from django.views.generic import DetailView, ListView, UpdateView, TemplateView, CreateView
+from remiolab.experiments.models import Experiment
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    """Home view"""
     template_name = "pages/home.html"
+    model = Experiment
+    paginate_by = 12
+    context_object_name = "experiments"
